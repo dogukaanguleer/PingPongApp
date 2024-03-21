98feba124c7e34e54a5e010cb59e17855607a282
dogukanguler <dogukan@cratustech.com>
2024-03-22 00:03:03 +0300

    edit

--- app/src/main/java/com/example/pingpong/KtorTestActivity.kt
+++ app/src/main/java/com/example/pingpong/KtorTestActivity.kt
@@ -2,16 +2,19 @@ package com.example.pingpong
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
+import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.plugins.websocket.cio.webSocketRawSession
 import io.ktor.client.plugins.websocket.webSocket
 import io.ktor.client.plugins.websocket.webSocketSession
 import io.ktor.client.request.header
@@ -23,6 +26,7 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.headers
 import io.ktor.websocket.Frame
 import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
 import io.ktor.websocket.readText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
@@ -34,9 +38,9 @@ import kotlinx.coroutines.withContext
 class KtorTestActivity : AppCompatActivity() {
 
     private val httpClient: HttpClient by lazy {
-        HttpClient(Android) {
+        HttpClient(CIO) {
             install(WebSockets) {
-                pingInterval = 10_000
+               // pingInterval = 10_000
             }
             install(Logging) {
                 logger = object : Logger {
@@ -55,17 +59,34 @@ class KtorTestActivity : AppCompatActivity() {
 
     private lateinit var textViewLog: TextView
     private lateinit var socket: WebSocketSession
+    private lateinit var buttonPing: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ktor_test)
-        //main()
-        initWebSocket()
+
+        textViewLog = findViewById(R.id.text_view_log)
+        buttonPing = findViewById(R.id.button_ping)
+
+        main()
+
+        //initWebSocket()
+
+
+        buttonPing.setOnClickListener {
+
+            lifecycleScope.launch {
+
+                if (::socket.isInitialized && socket.isActive)
+                    socket.send(Frame.Ping(byteArrayOf()))
+            }
+
+        }
     }
 
     override fun onDestroy() {
         lifecycleScope.launch(Dispatchers.IO) {
-            //socket.close()
+            socket.close()
             httpClient.close()
         }
         super.onDestroy()
@@ -87,76 +108,92 @@ class KtorTestActivity : AppCompatActivity() {
                 while (true) {
                     when (val frame = incoming.receive()) {
                         is Frame.Binary -> withContext(Dispatchers.Main) {
-                            textViewLog.append("Binary")
+                            textViewLog.append("Binary\n")
                         }
 
                         is Frame.Close -> withContext(Dispatchers.Main) {
-                            textViewLog.append("Close")
+                            textViewLog.append("Close\n")
                         }
 
                         is Frame.Ping -> withContext(Dispatchers.Main) {
-                            textViewLog.append("Ping")
+                            textViewLog.append("Ping\n")
                         }
 
                         is Frame.Pong -> withContext(Dispatchers.Main) {
-                            textViewLog.append("Pong")
+                            textViewLog.append("Pong\n")
                         }
 
                         is Frame.Text -> withContext(Dispatchers.Main) {
-                            textViewLog.append(frame.readText())
+                            textViewLog.append("${frame.readText()}\n")
                         }
                     }
                 }
             }
         } catch (e: Exception) {
             withContext(Dispatchers.Main) {
-                textViewLog.append(e.message)
+                textViewLog.append("${e.message}\n")
             }
             httpClient.close()
         }
+
+
+
     }
 
     private fun main() {
         lifecycleScope.launch(Dispatchers.IO) {
-            socket = httpClient.webSocketSession {
-                url("ws://10.0.0.138/ws/dali/devices")
-                headers {
-                    append("username", "atxled")
-                    append("password", "atxled")
-                }
-            }
-            if (socket.isActive) withContext(Dispatchers.Main) {
-                textViewLog.append("Connected")
-            }
-        }
-
-        lifecycleScope.launch(Dispatchers.IO) {
-            socket.incoming.consumeAsFlow().collectLatest { frame ->
-                when (frame) {
-                    is Frame.Text -> {
-                        withContext(Dispatchers.Main) {
-                            textViewLog.append("Received: ${frame.readText()}")
-                        }
-                    }
-
-                    is Frame.Ping -> {
-                        withContext(Dispatchers.Main) {
-                            textViewLog.append("Ping")
-                        }
-
-                    }
-
-                    is Frame.Pong -> {
-                        withContext(Dispatchers.Main) {
-                            textViewLog.append("Pong")
-                        }
-                    }
-
-                    else -> {
-
+            try {
+                socket = httpClient.webSocketRawSession() {
+                    url("ws://10.0.0.138/ws/dali/devices")
+                    headers {
+                        append("username", "atxled")
+                        append("password", "atxled")
                     }
                 }
+                if (socket.isActive) withContext(Dispatchers.Main) {
+                    textViewLog.append("Connected\n")
+
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        socket.incoming.consumeAsFlow().collectLatest { frame ->
+                            when (frame) {
+                                is Frame.Text -> {
+                                    withContext(Dispatchers.Main) {
+                                        textViewLog.append("Received: ${frame.readText()}\n")
+                                    }
+                                }
+
+                                is Frame.Ping -> {
+                                    withContext(Dispatchers.Main) {
+                                        textViewLog.append("Ping\n")
+                                    }
+
+                                }
+
+                                is Frame.Pong -> {
+                                    withContext(Dispatchers.Main) {
+                                        textViewLog.append("Pong\n")
+                                    }
+                                }
+
+                                else -> {
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }catch (e:Exception){
+                withContext(Dispatchers.Main) {
+                    textViewLog.append("${e.message}\n")
+                }
+                httpClient.close()
+
             }
+
+
+
         }
+
+
     }
 }
