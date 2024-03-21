76ce704bfe5686cbdb422c5538a1cd9de22b0555
Onat Akduman <onat@cratustech.com>
2024-03-21 22:41:33 +0300

    add engine

--- app/src/main/java/com/example/pingpong/KtorTestActivity.kt
+++ app/src/main/java/com/example/pingpong/KtorTestActivity.kt
@@ -13,37 +13,39 @@ import io.ktor.client.request.url
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.websocket.Frame
-import io.ktor.websocket.FrameType
 import io.ktor.websocket.WebSocketSession
 import io.ktor.websocket.close
 import io.ktor.websocket.readText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class KtorTestActivity : AppCompatActivity() {
 
+    private val httpClient: HttpClient by lazy {
+        HttpClient(CIO) {
+            install(WebSockets) {
+                pingInterval = 10_000
+            }
+        }
+    }
 
-    private lateinit var webSocketClient: HttpClient
     private lateinit var textViewLog: TextView
-    private var socket: WebSocketSession? = null
+    private lateinit var socket: WebSocketSession
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ktor_test)
-        webSocketClient = HttpClient(CIO) {
-            install(WebSockets) {
-                pingInterval = 5L
-            }
-        }
         main()
     }
 
     override fun onDestroy() {
         lifecycleScope.launch(Dispatchers.IO) {
-            socket?.close()
+            socket.close()
+            httpClient.close()
         }
         super.onDestroy()
     }
@@ -51,7 +53,7 @@ class KtorTestActivity : AppCompatActivity() {
     private fun main() {
 
         lifecycleScope.launch(Dispatchers.IO) {
-            socket = webSocketClient.webSocketSession {
+            socket = httpClient.webSocketSession {
                 url("ws://10.0.0.138/ws/dali/devices")
                 headers {
                     append("username", "atxled")
@@ -59,13 +61,15 @@ class KtorTestActivity : AppCompatActivity() {
                 }
                 contentType(ContentType.Application.Json)
             }
+            if (socket.isActive) withContext(Dispatchers.Main) {
+                textViewLog.append("Connected")
+            }
         }
 
         lifecycleScope.launch(Dispatchers.IO) {
-            socket?.incoming?.consumeAsFlow()?.collectLatest { frame ->
+            socket.incoming.consumeAsFlow().collectLatest { frame ->
                 when (frame) {
                     is Frame.Text -> {
-
                         withContext(Dispatchers.Main) {
                             textViewLog.append("Received: ${frame.readText()}")
                         }
