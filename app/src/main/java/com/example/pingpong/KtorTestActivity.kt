679622f253c3354d6d1fa9a73a779c366b97c172
Onat Akduman <onat@cratustech.com>
2024-03-21 22:21:41 +0300

    update project

--- app/src/main/java/com/example/pingpong/KtorTestActivity.kt
+++ app/src/main/java/com/example/pingpong/KtorTestActivity.kt
@@ -1,71 +1,88 @@
 package com.example.pingpong
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.plugins.websocket.webSocket
-import io.ktor.http.HttpMethod
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.client.request.headers
+import io.ktor.client.request.url
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
 import io.ktor.websocket.readText
-import kotlinx.coroutines.runBlocking
-import kotlin.time.Duration
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.launch
 
 class KtorTestActivity : AppCompatActivity() {
 
 
     private lateinit var webSocketClient: HttpClient
     private lateinit var textViewLog: TextView
+    private var socket: WebSocketSession? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ktor_test)
-        main()
-    }
-
-    override fun onDestroy() {
-        webSocketClient.close()
-        super.onDestroy()
-    }
-
-    private fun main() {
         webSocketClient = HttpClient {
             install(WebSockets) {
                 pingInterval = 5L
             }
         }
+        main()
+    }
 
-        runBlocking {
-            webSocketClient.webSocket(
-                method = HttpMethod.Get,
-                host = "10.0.0.138",
-                port = 80,
-                path = "/ws/dali/devices"
-            ) {
-                for (frame in incoming) {
-                    when (frame) {
-                        is Frame.Text -> {
+    override fun onDestroy() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            socket?.close()
+        }
+        super.onDestroy()
+    }
 
-                            runOnUiThread {
-                                textViewLog.append("Received: ${frame.readText()}")
-                            }
+    private fun main() {
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            socket = webSocketClient.webSocketSession {
+                url("ws://10.0.0.138/ws/dali/devices")
+                headers {
+                    append("username", "atxled")
+                    append("password", "atxled")
+                }
+                contentType(ContentType.Application.Json)
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            socket?.incoming?.consumeAsFlow()?.collectLatest { frame ->
+                when (frame) {
+                    is Frame.Text -> {
+
+                        runOnUiThread {
+                            textViewLog.append("Received: ${frame.readText()}")
                         }
-                        is Frame.Ping -> {
-                            runOnUiThread {
-                                textViewLog.append("Ping")
-                            }
+                    }
 
-                        }
-                        is Frame.Pong -> {
-                            runOnUiThread {
-                                textViewLog.append("Pong")
-                            }
+                    is Frame.Ping -> {
+                        runOnUiThread {
+                            textViewLog.append("Ping")
                         }
 
-                        else -> {
+                    }
 
+                    is Frame.Pong -> {
+                        runOnUiThread {
+                            textViewLog.append("Pong")
                         }
+                    }
+
+                    else -> {
+
                     }
                 }
             }
